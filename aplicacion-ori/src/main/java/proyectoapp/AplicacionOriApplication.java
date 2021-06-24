82efb6dd6aea19cfb34674b8cafa79d769864b06
Max30031998 <america_tego@hotmail.com>
2021-06-24 12:13:54 -0500

    comit inicial con eclipse

---
+++ aplicacion-ori/src/main/java/proyectoapp/AplicacionOriApplication.java
@@ -0,0 +1,13 @@
+package proyectoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionOriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionOriApplication.class, args);
+	}
+
+}
---
+++ aplicacion-ori/src/test/java/proyectoapp/AplicacionOriApplicationTests.java
@@ -0,0 +1,13 @@
+package proyectoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionOriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
